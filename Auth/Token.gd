464a6de0d896eb22c0fe028308c71b47de87c461
galo <>
2021-05-15 21:37:37 -0300

    token being send from login to server

--- Auth/Token.gd
+++ Auth/Token.gd
@@ -1,22 +1,20 @@
 extends Node
 
-var file = File.new()
 var packet = StreamPeerTCP.new()
 var data
 var string
 var json2
-var json = {"test":"123"}
+var json = {"token":""}
 
-func _ready():
+func send_data(token):
 	packet.connect_to_host( "::1", 8082)
-	print("connected")
-
-func _physics_process(delta):
+	print("Token: " + str(token) + " was sent")
 	var peerstream = PacketPeerStream.new()
 	peerstream.set_stream_peer(packet)
 	if peerstream.get_available_packet_count() > 0:
 		data = (peerstream.get_packet())
 		string = data.get_string_from_ascii()
 		json2 = parse_json(string)
-	else:
-		packet.put_string(to_json(json))
+	packet.put_string(str(token))
+	packet.disconnect_from_host()
+
