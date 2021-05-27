c2f0690c67f6f7124bf46fd043d70d950f43d4b2
galo <>
2021-05-27 18:24:33 -0300

    dowloand patches from the internet 2

--- Launcher/dowloanndtext.gd
+++ Launcher/dowloanndtext.gd
@@ -1,15 +1,41 @@
 extends Node
 
+var currentversion = 100
+var status = 0
+var downloaded_bytes = 0
+var body_size = 0
+
 func _ready():
-	$HTTPRequest.set_download_file('user://patches/patch.pck')
-	$HTTPRequest.request('https://git.galodev.net/galo/gittest/raw/master/backpack.pck')
+	var directory = Directory.new()
+	if directory.dir_exists("user://patches"):
+		pass
+	else:
+		directory.make_dir("user://patches")
+	$HTTPRequest2.request('https://git.galodev.net/galo/gittest/raw/master/version.json')
+
+func dowloadupdate():
+	print('dowloading patch')
+	$HTTPRequest.set_download_file('user://patches/newpatch.pck')
+	$HTTPRequest.request('https://git.galodev.net/galo/gittest/raw/master/MMOlite.pck')
 
 func _on_HTTPRequest_request_completed(result, response_code, headers, body):
 	if response_code == 200:
-		var directory = Directory.new()
-		if directory.dir_exists("user://patches"):
-			pass
-		else:
-			directory.make_dir("user://patches")
-		ProjectSettings.load_resource_pack('user://patches/patch.pck')
-		get_tree().change_scene_to(load("res://Tests/TestZone.tscn"))
+		ProjectSettings.load_resource_pack('user://patches/newpatch.pck')
+		get_tree().change_scene_to(load("res://Client/Client.tscn"))
+
+func _on_HTTPRequest2_request_completed(result, response_code, headers, body):
+	var data = JSON.parse(body.get_string_from_ascii())
+	var dataprocess = data.result
+	print(body.get_string_from_ascii())
+	print(dataprocess)
+	body_size = dataprocess['size']
+	if dataprocess['version'] > currentversion:
+		dowloadupdate()
+	else:
+		print('no version to dowload')
+	pass # Replace with function body.
+
+func _process(delta):
+	status = $HTTPRequest.get_http_client_status()
+	downloaded_bytes = $HTTPRequest.get_downloaded_bytes()
+	$Label.text = 'dowloaded '+str(downloaded_bytes/1024) +' kbs of '+ str(body_size) +' kbs'
